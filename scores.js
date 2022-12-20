49ad67b099ef15a70d1c6ff69ad8dc519e87263b
chirly sfez <chirlysfez17@gmail.com>
2022-12-21 01:44:15 +0200

    scores

--- scores.js
+++ scores.js
@@ -3,20 +3,46 @@
 // // ✅ Change (replace) the text of the element
 // heading.textContent ='Replacement heading text';
 
-
-
-function addRow(){
+function Hi(){
     document.write(`<h1>Hi ${localStorage.getItem('current user')}</h1>`);
+
+}
+
+function show_scores(){
+   // document.write(`<h1>Hi ${localStorage.getItem('current user')}</h1>`);
     //document.write(markup);
     //var obj=Object.entries(localStorage).filter(([key])=>key.startsWith('score'));
     //console.log(obj);
+    score_platformer=[];
+    score_2048=[];
     for(let i=0;i<localStorage.length;i++){
         var key=localStorage.key(i);
-        if(key.startsWith('score')){
-            document.write(key+": "+ localStorage.getItem(key));
-            document.write('<br>');
+        var name=key.split(" ");
+        if(key.startsWith('score platformer')){
+            score_platformer+=`${name[name.length-1]}: ${localStorage.getItem(key)}`;
+            
+            // document.write(key+": "+ localStorage.getItem(key));
+            // document.write('<br>');
         }
+        if(key.startsWith('score 2048')){
+            score_2048+=`${name[name.length-1]}: ${localStorage.getItem(key)}`;
+            
+            // document.write(key+": "+ localStorage.getItem(key));
+            // document.write('<br>');
+        }
+        
     }
+    document.write('<center>');
+    document.write('<h2>scores platformer</h2>');
+    for(let i=0;i<score_platformer.length;i++){
+         document.write(score_platformer[i]);
+    }
+
+    document.write('<h2>scores 2048</h2>');
+    for(let i=0;i<score_2048.length;i++){
+        document.write(score_2048[i]);
+    }
+    document.write('</center>');
     
     // let table = document.getElementById("TableScore");
     // let row = document.createElement("tr");
