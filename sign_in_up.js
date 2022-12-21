520e702359da24205e3f06f0c8aa0e8fb8b7896c
Léa Scetbon <scetbon@g.jct.ac.il>
2022-12-21 13:12:24 +0200

    home title

--- sign_in_up.js
+++ sign_in_up.js
@@ -76,37 +76,7 @@ function check(){
 
 }
 
-//cookies
-//document.cookie = "username=scetbonlea@gmail.com; expires=Thu, 5 Jan 2023 12:00:00 UTC";
-//console.log(document.cookie);
 
-function setCookie(name, value, daysToLive) {
-    const date = new Date();
-    date.setTime(date.getTime() + (daysToLive * 24 * 60 * 60 * 1000));
-    let expires = "expires="+date.toUTCString();
-    document.cookie = name + "=" + value + ";" + daysToLive + ";path=/";
-  }
-
-  setCookie("email","scetbonlea@gmail.com", 365),
-  console.log(getCookies("email"));
-
-  function deleteCookie(name){
-    setCookies(name,null,null);
-  }
-
-  function getCookies(name){
-    const cDecoded = decodeURIComponent(document.cookies);
-    const cArray = cDecoded.split("; ");
-    let result;
-
-    cArray.forEach(element=> {
-        if(element.indexOf(name) == 0){
-        result = element.substring(name.length + 1)
-    }
-
-  })
-  return result;
-}
   
   
   
